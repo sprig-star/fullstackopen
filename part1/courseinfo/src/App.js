3c2e8a7dd03e74e8386a69219c87726eff82e1a5
Ro <ro@openform.co>
2022-07-05 19:13:23 -0700

    exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,18 +7,16 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
     </>
   )
 }
+
+const Part = (props) => (
+  <p>{props.name} {props.exercises}</p>
+)
 
 const Total = (props) => {
   return (
