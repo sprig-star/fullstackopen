afaf27245545a555b02c3eba95eb830322700321
Ro <ro@openform.co>
2022-07-08 15:11:10 -0700

    exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -27,6 +27,9 @@ const App = () => {
       <Display text='good' value={good} />
       <Display text='neutral' value={neutral} />
       <Display text='bad' value={bad} />
+      <Display text='all' value={good + neutral + bad} />
+      <Display text='average' value={(good - bad) / (good + neutral + bad)} />
+      <Display text='positive' value={good / (good + neutral + bad)} />
     </div>
   )
 }
