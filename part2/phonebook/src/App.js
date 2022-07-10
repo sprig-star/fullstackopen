336682e6a239b9d1597bdb27aca6c32b2ec0779e
Ro <ro@openform.co>
2022-07-10 13:52:51 -0700

    exercise 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,41 @@
 import { useState } from 'react'
 
+const Search = ({query, onChange}) =>
+  <form>
+    <h3>search</h3>
+    filter by name:
+    <input type="search"
+      value={query}
+      onChange={onChange} 
+    />
+  </form>
+
+const PersonsForm = ({name, nameHandler, number, numberHandler, submitHandler}) =>
+  <form>
+    <h3>add a person</h3>
+    name:
+      <input type="text" 
+        value={name} 
+        onChange={nameHandler}
+      />
+    number:
+      <input type="tel" 
+        value={number} 
+        onChange={numberHandler}
+      />
+    <button type="submit" onClick={submitHandler}>add</button>
+  </form>
+
 const Person = ({person}) =>
   <li>{person.name}, {person.number}</li>
+
+const PhoneList = ({persons}) =>
+  <>
+    <h2>Numbers</h2>
+    <ul>
+      {persons.map(person => <Person key={person.name} person={person} />)}
+    </ul>
+  </>
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -38,32 +72,18 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <h3>search</h3>
-      <form>
-        filter name:
-        <input type="search"
-          value={searchQuery}
-          onChange={(event) => setSearchQuery(event.target.value)} 
-        />
-      </form>
-      <h3>add a person</h3>
-      <form>
-        name:
-          <input type="text" 
-            value={newName} 
-            onChange={(event) => setNewName(event.target.value)}
-          />
-        number:
-          <input type="tel" 
-            value={newNumber} 
-            onChange={(event) => setNewNumber(event.target.value)}
-          />
-        <button type="submit" onClick={addPerson}>add</button>
-      </form>
-      <h2>Numbers</h2>
-        <ul>
-          {personsToShow.map(person => <Person key={person.name} person={person} />)}
-        </ul>
+      <Search
+        query={searchQuery}
+        onChange={(event) => setSearchQuery(event.target.value)} 
+      />
+      <PersonsForm
+        name={newName}
+        nameHandler = {(event) => setNewName(event.target.value)}
+        number={newNumber} 
+        numberHandler = {(event) => setNewNumber(event.target.value)}
+        submitHandler = {addPerson}
+      />
+      <PhoneList persons={personsToShow} />
     </div>
   )
 }
