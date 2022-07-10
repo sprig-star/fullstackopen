4a903f3c546aabbd88c74d6bfa6377ac8e870663
Ro <ro@openform.co>
2022-07-10 13:20:55 -0700

    exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,35 +1,39 @@
 import { useState } from 'react'
 
 const Person = ({person}) =>
-  <li>{person.name}</li>
+  <li>{person.name}, {person.number}</li>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '39-44-5323523'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.some(entry => entry.name === newName)){
+    if (newName.length === 0 || newNumber.length === 0){
+      window.alert(`Please complete the entry`)
+    }
+    else if (persons.some(entry => entry.name === newName)){
       window.alert(`${newName} is already in the phonebook`)
     }
     else {
-      setPersons(persons.concat({name: newName}))
+      setPersons(persons.concat({name: newName, number: newNumber}))
     }
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
-        <div>
-          name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
-        </div>
-        <div>
-          <button type="submit" onClick={addPerson}>add</button>
-        </div>
+        name: <input type="text" value={newName} onChange={(event) => setNewName(event.target.value)}/>
+        number: <input type="tel" value={newNumber} onChange={(event) => setNewNumber(event.target.value)}/>
+        <button type="submit" onClick={addPerson}>add</button>
       </form>
       <h2>Numbers</h2>
         <ul>
