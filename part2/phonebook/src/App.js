f7620adea2965168d2a4d14f5a0d437c5ad8b1e0
Ro <ro@openform.co>
2022-07-11 17:12:59 -0700

    exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Search = ({query, onChange}) =>
   <form>
@@ -38,15 +39,18 @@ const PhoneList = ({persons}) =>
   </>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
+  console.log('rendered', persons.length, 'people')
 
   const personsToShow = persons.filter( person => 
     person.name.toLowerCase().includes(searchQuery.toLowerCase())
