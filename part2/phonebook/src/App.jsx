539822a0b32c04f8f843b71dce71c00b4f86e39a
Ro <ro@openform.co>
2022-07-16 23:42:35 -0700

    exercises 2.19-20

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,23 @@
 import { useState, useEffect } from 'react'
 import contactService from './services/contactService'
 
+const Notification = ({message, closeNotification}) => {
+  const notification = {
+    backgroundColor: 'pink',
+    padding: 20
+  }
+  const close = {
+    display: 'block',
+    fontSize: 12
+  }
+  if (message) return (
+    <aside style={notification}>
+      {message}
+      <a style={close} onClick={closeNotification} href=''>Dismiss notification</a>
+    </aside>
+  )
+}
+
 const Search = ({query, onChange}) =>
   <form>
     <h3>search</h3>
@@ -43,14 +60,21 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
+  const [notification, setNotification] = useState(null)
 
   useEffect(() => {
     console.log('effect')
     contactService
       .getAll()
       .then(responseData => setPersons(responseData))
+      .then(setNotification('Contacts loaded from server'))
   }, [])
   console.log('rendered', persons.length, 'people')
+
+  const closeNotification = event => {
+    event.preventDefault()
+    setNotification(null)
+  }
 
   const personsToShow = persons.filter( person => 
     person.name.toLowerCase().includes(searchQuery.toLowerCase())
@@ -72,6 +96,11 @@ const App = () => {
         contactService
           .update(existingPerson.id, {...existingPerson, number: newNumber})
           .then(changedPerson => setPersons(persons.map((person, id) => id === nameIndex ? changedPerson : person)))
+          .then(setNotification(`Updated ${newName}`))
+          .catch(error => {
+            setNotification(`Contact could not be updated, ${newName} was previously deleted`)
+            setPersons(persons.filter((p, i) => i !== nameIndex))
+          })
       }
     }
     else {
@@ -79,6 +108,7 @@ const App = () => {
       contactService
         .create(newPerson)
         .then(responseData => setPersons(persons.concat(responseData)))
+        .then(setNotification(`Added ${newName}`))
     }
     setNewName('')
     setNewNumber('')
@@ -90,12 +120,14 @@ const App = () => {
       contactService
         .remove(person.id)
         .then(() => setPersons(persons.slice(0, indexOfId).concat(persons.slice(indexOfId+1))))
+        .then(setNotification(`Deleted ${person.name}`))
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification} closeNotification={closeNotification} />
       <Search
         query={searchQuery}
         onChange={(event) => setSearchQuery(event.target.value)} 
